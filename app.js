4cf450f0c0aab516788a4df3705489b0179afdfb
Kornelos <kornel1902@gmail.com>
2019-12-28 13:16:59 +0100

    peja

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${PORT}/`);
 });
