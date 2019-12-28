41bdaacee3643425f37f9d2314c7562bd5428e65
Kornelos <kornel1902@gmail.com>
2019-12-28 13:13:07 +0100

    peja

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +10,6 @@ const server = http.createServer((req, res) => {
   res.end('Rychu Peja chce tu uczciwie zarobic\n');
 });
 
-server.listen(port, hostname, () => {
+server.listen(PORT, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
