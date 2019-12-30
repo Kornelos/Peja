d9f3861bba6d0056fb005148d8d375358a607c92
Kornelos <kornel1902@gmail.com>
2019-12-30 23:27:27 +0100

    peja

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-import Genius from 'genius-api'
+const Genius = require('genius-api');
+const fetch = require("node-fetch");
 
-const accessToken = process.env.GENIUS_TOKEN
-const genius = new Genius(accessToken)
+const accessToken = process.env.GENIUS_TOKEN;
+
+const genius = new Genius(accessToken);
 
 
 const http = require('http');
-const hostname = process.env.YOUR_HOST || '0.0.0.0';
+const hostname = process.env.YOUR_HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
 // genius API does not have an artist entrypoint.
 // Instead, search for the artist => get a song by that artist => get API info on that song => get artist id
 Genius.prototype.getArtistIdByName = function getArtistIdByName(artistName) {
-  const normalizeName = name => name.replace(/\./g, '').toLowerCase()   // regex removes dots
-  const artistNameNormalized = normalizeName(artistName)
+  const normalizeName = name => name.replace(/\./g, '').toLowerCase();   // regex removes dots
+  const artistNameNormalized = normalizeName(artistName);
 
   return this.search(artistName)
     .then((response) => {
       for (let i = 0; i < response.hits.length; i += 1) {
-        const hit = response.hits[i]
+        const hit = response.hits[i];
         if (hit.type === 'song' && normalizeName(hit.result.primary_artist.name) === artistNameNormalized) {
           return hit.result
         }
@@ -25,7 +27,7 @@ Genius.prototype.getArtistIdByName = function getArtistIdByName(artistName) {
       throw new Error(`Did not find any songs whose artist is "${artistNameNormalized}".`)
     })
     .then(songInfo => songInfo.primary_artist.id)
-}
+};
 
 
 
@@ -35,41 +37,60 @@ Genius.prototype.getSongLyrics = function getSongLyrics(geniusUrl) {
     method: 'GET',
   })
   .then(response => {
-    if (response.ok) return response.text()
+    if (response.ok) return response.text();
     throw new Error('Could not get song url ...')
   })
   .then(parseSongHTML)
-}
+};
 
 // parse.js
-import cheerio from 'cheerio'
+const cheerio = require('cheerio');
+
 
 function parseSongHTML(htmlText) {
-  const $ = cheerio.load(htmlText)
-  const lyrics = $('.lyrics').text()
-  const releaseDate = $('release-date .song_info-info').text()
+  const $ = cheerio.load(htmlText);
+  const lyrics = $('.lyrics').text();
+  const releaseDate = $('release-date .song_info-info').text();
   return {
     lyrics,
     releaseDate,
   }
 }
 
-const genius = new Genius(accessToken)
-genius.getArtistIdByName('Peja')
-.then(artistId => { /* ... */ })
-.catch(err => console.error(err))
+//const genius = new Genius(accessToken)
+function getPeja(completionHandler) {
+    genius.getArtistIdByName('Peja')
+        .then(artistId => {
+            genius.songsByArtist(artistId, {
+                per_page: 50,
+                sort: 'popularity',
+            })
+                .then(songs => {
+                    const urls = songs.songs.map(song => song.url);
+                    // console.log(songs);
+                    const randomNumber = Math.floor(Math.random() * 51);
+                    genius.getSongLyrics(urls[randomNumber])
+                        .then(text => {
+                            console.log(text)
+                            completionHandler(text.lyrics.toString());
+                        })
+                })
+                .catch(err => console.error(err));
 
-genius.songsByArtist(artistId, {
-    per_page: 50,
-    sort: 'popularity',
-})
-.then(songs => songs.map(song => song.url)) // has more song info like 'id', 'title', ...
-console.log(songs);
+
+        })
+        .catch(err => console.error(err));
+}
+
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Rychu Peja chce tu uczciwie zarobic\n');
+
+  getPeja(function (text) {
+      res.statusCode = 200;
+     // res.setHeader('Content-Type', 'text/plain');
+      res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      res.end(text);
+  })
 });
 
 server.listen(PORT, hostname, () => {
